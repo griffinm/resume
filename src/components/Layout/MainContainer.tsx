a94f590b34e46a327b53f8925415854f07bccc19
Griffin Mahoney <griffin@yardzen.com>
2025-05-07 21:50:07 -0400

    wip

--- src/components/Layout/MainContainer.tsx
+++ src/components/Layout/MainContainer.tsx
@@ -1,7 +1,7 @@
 export function MainContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-background text-text-main font-sans antialiased">
-      <div className="resume-container max-w-4xl mx-auto my-8 p-8 bg-white shadow-lg print:shadow-none print:my-0 print:mx-0 print:p-10">
+    <div className="bg-neutral-100 text-text-main font-sans antialiased pt-8">
+      <div className="resume-container max-w-4xl mx-auto p-8 bg-white shadow-xl print:shadow-none print:my-0 print:mx-0 print:p-10">
         {children}
       </div>
     </div>
