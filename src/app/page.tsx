ff6d414a8816c6d06bc33e1f6e15b189353f75d8
Griffin <griffin.mahoney@gmail.com>
2025-10-22 16:03:25 -0400

    Re arranging buttons on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,9 +43,9 @@ export default function HomePage() {
 
           {/* Budgeting Project Button */}
           <HomeButton
-            href="/projects/budgeting"
-            title="Budgeting"
-            description="Personal finance management tool"
+            href="/projects/"
+            title="View My Projects"
+            description="A collection of personal projects I've built"
             gradientFrom="from-purple-500"
             gradientTo="to-purple-700"
             hoverGradientFrom="from-purple-600"
@@ -61,32 +61,7 @@ export default function HomePage() {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                <path d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-            }
-          />
-
-          {/* Notes Project Button */}
-          <HomeButton
-            href="/projects/notes"
-            title="Notes"
-            description="Note-taking and organization app"
-            gradientFrom="from-green-500"
-            gradientTo="to-green-700"
-            hoverGradientFrom="from-green-600"
-            hoverGradientTo="to-green-800"
-            textColor="text-green-100"
-            icon={
-              <svg
-                className="w-12 h-12 text-white"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                <path d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
               </svg>
             }
           />
